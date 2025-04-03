d31736a33591394cecc81f588e7676fd916e825a
MohammedOsamma <mo856915i@gmail.com>
2025-04-03 16:54:22 +0200

    Add editCertificate function in certificate controller  with Routes

--- controller/admin/certificate-controller.js
+++ controller/admin/certificate-controller.js
@@ -39,7 +39,39 @@ const fetchAllCert = async (req, res) => {
   }
 };
 
+const editCertificate = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { image, name } = req.body;
+    let findCert = await Certificate.findById(id);
+
+    if (!findCert) {
+      return res.status(404).json({
+        success: false,
+        message: "Certificate not Found!",
+      });
+    }
+
+    findCert.image = image || findCert.image;
+    findCert.name = name || findCert.name;
+
+    await findCert.save();
+
+    res.status(200).json({
+      success: true,
+      data: findCert,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: "Error Occured",
+    });
+  }
+};
+
 module.exports = {
   addCertificate,
   fetchAllCert,
+  editCertificate,
 };
--- routes/admin/certificate-routes.js
+++ routes/admin/certificate-routes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const {
   addCertificate,
   fetchAllCert,
+  editCertificate,
 } = require("../../controller/admin/certificate-controller");
 
 router.post("/add-cert", addCertificate);
 router.get("/get-certs", fetchAllCert);
+router.put("/edit/:id", editCertificate);
 
 module.exports = router;
