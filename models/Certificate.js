01287240107da55dd692cc2251802011e46fb76e
MohammedOsamma <mo856915i@gmail.com>
2025-04-03 13:38:18 +0200

    Add timestamps to our models

--- models/Certificate.js
+++ models/Certificate.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
 
-const certificateSchema = new mongoose.Schema({
-  image: String,
-  name: String,
-  certLink: String,
-});
+const certificateSchema = new mongoose.Schema(
+  {
+    image: String,
+    name: String,
+    certLink: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Certificate", certificateSchema);
--- models/Project.js
+++ models/Project.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
 
-const projectSchema = new mongoose.Schema({
-  image: String,
-  title: String,
-  skills: [{ type: String }],
-  githubLink: String,
-  demoLink: String,
-});
+const projectSchema = new mongoose.Schema(
+  {
+    image: String,
+    title: String,
+    skills: [{ type: String }],
+    githubLink: String,
+    demoLink: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Project", projectSchema);
